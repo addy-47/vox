e0a8fee9a03c53c68f06b4d2e5f7e97f983cb3a0
addy-47 <adhbutg@gmail.com>
2026-05-05 22:43:14 +0530

    patched position blugin bug in wayland ,using modified plugin , pushed pr to open source code as well

---
+++ app/src-tauri/plugins/tauri-plugin-positioner/build.rs
@@ -0,0 +1,10 @@
+const COMMANDS: &[&str] = &[
+    "move_window",
+    "move_window_constrained",
+    "set_tray_icon_state",
+];
+
+fn main() {
+    tauri_plugin::Builder::new(COMMANDS)
+        .build();
+}
